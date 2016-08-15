24cdc6b1428c211b2ce53c12103c2a1bec5c493b
Xu Lei <a871460434@gmail.com>
2016-08-15 16:40:20 +0800

    remove hmr

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,7 +10,7 @@ module.exports = {
     ],
     output: {
         path: path.join(__dirname, 'static'),
-        filename: 'bundle.js',
+        filename: 'bundle.min.js',
         publicPath: '/static/'
     },
     plugins: [
