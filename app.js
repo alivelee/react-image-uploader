3c618f7660e1eb7ed4ed1ba8ea243ef0310697f7
Xu Lei <a871460434@gmail.com>
2016-08-15 16:56:15 +0800

    fix express server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-
-app.use(express.static(__dirname + 'static'));
+const indexPath = path.join(__dirname, 'index.html');
+const publicPath = express.static(path.join(__dirname, 'static'));
+app.use('/static', publicPath);
+app.get('/', function (_, res) { res.sendFile(indexPath) });
 
 app.listen(process.env.PORT || 8080);
